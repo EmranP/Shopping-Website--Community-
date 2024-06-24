4df6d0d3467f63c2b4a88b6693812061dc8891eb
Emran <nightsking12345@gmail.com>
2024-06-24 14:02:01 +0500

    first commit

---
+++ src/components/header/Navigation.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useRef, useState } from 'react'
+import '../../scss/header.scss'
+import { navigationList } from '../../utils/navbar'
+import { Btn } from '../btn'
+// eslint-disable-next-line react/prop-types
+export const Navigation = ({ headerRefProp }) => {
+	const [isOpen, setIsOpen] = useState(false)
+	const burgerRef = useRef(null)
+	const menuListRef = useRef(null)
+
+	const clickBurgerHandle = () => {
+		if (isOpen) {
+			menuListRef.current.classList.toggle('menu__active')
+			burgerRef.current.classList.toggle('burger__active')
+			headerRefProp.current.classList.toggle('header__active')
+		}
+	}
+
+	return (
+		<>
+			<div
+				onClick={() => {
+					setIsOpen(true)
+					clickBurgerHandle()
+				}}
+				ref={burgerRef}
+				className='menu__burger'
+			>
+				<span></span>
+			</div>
+
+			<nav ref={menuListRef} className='header__menu'>
+				<ul className='menu__list'>
+					{navigationList.map(item => (
+						<li key={item.id}>{item.title}</li>
+					))}
+				</ul>
+				<Btn title={'sign up'} />
+			</nav>
+		</>
+	)
+}
